32ebab8c9579048fc7904684f4d9bcd1a54eb5d5
punjitha <punjitha@gmail.com>
2025-06-20 14:19:51 +0530

    added a for loop and logic to loop and ask user for reservations until tickets run out

--- main.go
+++ main.go
@@ -15,27 +15,36 @@ func main(){
 	fmt.Printf("We have a total of %v tickets out of %v tickets.\n", remainigTickets, conferenceTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
+	for {
 
-	fmt.Println("\nEnter your first name:")
-	fmt.Scan(&firstName)
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	fmt.Println("\nEnter your last name:")
-	fmt.Scan(&lastName)
+		fmt.Println("\nEnter your first name:")
+		fmt.Scan(&firstName)
 
-	fmt.Println("\nEnter your email:")
-	fmt.Scan(&email)
+		fmt.Println("\nEnter your last name:")
+		fmt.Scan(&lastName)
 
-	fmt.Println("\nEnter the number of tickets you want:")
-	fmt.Scan(&userTickets)
+		fmt.Println("\nEnter your email:")
+		fmt.Scan(&email)
 
-	remainigTickets = remainigTickets - userTickets
-	bookings = append(bookings, firstName + " " + lastName)
+		fmt.Println("\nEnter the number of tickets you want:")
+		fmt.Scan(&userTickets)
 
-	fmt.Printf("\nThank You %v %v,\n%v tickets will be sent to your email: %v shortly.\n", firstName, lastName, userTickets, email)
-	fmt.Printf("\nRemainig Tickets: %v\n", remainigTickets)
-	fmt.Printf("Bookings: %v\n", bookings)
+		remainigTickets = remainigTickets - userTickets
+		bookings = append(bookings, firstName + " " + lastName)
+
+		fmt.Printf("\nThank You %v %v,\n%v tickets will be sent to your email: %v shortly.\n", firstName, lastName, userTickets, email)
+		fmt.Printf("\nRemainig Tickets: %v\n", remainigTickets)
+		fmt.Printf("Bookings: %v\n", bookings)
+
+		if remainigTickets == 0 {
+			fmt.Println("No tickets availbale.")
+			break
+		}
+
+	}
 }
